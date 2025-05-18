4cce13855c25c9fc369824cd823fa63a53bea50c
IlCai116 <palinsesto99@gmail.com>
2025-05-18 13:31:20 +0200

    Creazione pagina visualizza-utili

--- Codice/Ciscusassi/src/app/pages/admin/visualizza-utili/visualizza-utili.page.ts
+++ Codice/Ciscusassi/src/app/pages/admin/visualizza-utili/visualizza-utili.page.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonRow, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-visualizza-utili',
   templateUrl: './visualizza-utili.page.html',
   styleUrls: ['./visualizza-utili.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonInput, IonButton, IonGrid, IonRow, IonCol,IonSelect, IonSelectOption]
 })
 export class VisualizzaUtiliPage implements OnInit {
 
@@ -16,5 +16,16 @@ export class VisualizzaUtiliPage implements OnInit {
 
   ngOnInit() {
   }
+
+  months = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+
+rows = [
+  { address: 'Via Vincenzo Piazza Martini, 45', values: Array(12).fill('- €') },
+  { address: 'Via Palmerino , 52/A', values: Array(12).fill('- €') },
+  { address: 'Via Saitta Longhi, 116G', values: Array(12).fill('- €') },
+  { address: 'Via Catania, 17', values: Array(12).fill('- €') },
+ 
+];
+years = [2025, 2024, 2023, 2022, 2021]; // Puoi anche generarli dinamicamente
 
 }
