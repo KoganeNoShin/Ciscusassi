e0de98aa2c56ca24498cf5c6d8d9b8ada320106f
KoganeNoShin <diecoro2003@gmail.com>
2025-05-20 12:31:09 +0200

    Prenota dal Database

--- Codice/Ciscusassi/src/app/pages/prenota/prenota.page.ts
+++ Codice/Ciscusassi/src/app/pages/prenota/prenota.page.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader} from '@ionic/angular/standalone';
+import { IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonSpinner} from '@ionic/angular/standalone';
 import { HeroComponent } from 'src/app/components/hero/hero.component';
 import { LeafletMapComponent } from 'src/app/components/leaflet-map/leaflet-map.component';
 
 import { Filiale } from 'src/app/core/interfaces/Filiale';
+import { PrenotaService } from './prenota.service';
 
 @Component({
   selector: 'app-prenota',
   templateUrl: './prenota.page.html',
   styleUrls: ['./prenota.page.scss'],
   standalone: true,
-  imports: [IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonContent, CommonModule, FormsModule, HeroComponent, LeafletMapComponent]
+  imports: [IonSpinner, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonContent, CommonModule, FormsModule, HeroComponent, LeafletMapComponent]
 })
 export class PrenotaPage implements OnInit {
 
-  //filiale = Filiale[] = [];
+  filiali: Filiale[] = [];
   loading: boolean = true;
 
-  constructor() { }
+  constructor(private prenotaService: PrenotaService) { }
 
   ngOnInit() {
+    this.prenotaService.GetSedi().subscribe({
+      next: (response) => {
+        console.log(response);
+        this.filiali = response;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.filiali = [];
+        this.loading = false;
+      }
+    })
   }
-
 }
--- Codice/Ciscusassi/src/app/pages/prenota/prenota.service.ts
+++ Codice/Ciscusassi/src/app/pages/prenota/prenota.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PrenotaService {
 
-  constructor() { }
+export class PrenotaService {
+  private apiURL = environment.apiURL;
+
+  constructor(private http: HttpClient) { }
+
+  GetSedi(): Observable<any> {
+    return this.http.get(`${this.apiURL}/Filiali`);
+  }
 }
