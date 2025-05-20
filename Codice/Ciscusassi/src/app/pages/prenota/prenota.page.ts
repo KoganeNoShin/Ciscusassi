70a4993108d84a8792b30fcd66bc2d1a4efdb934
KoganeNoShin <diecoro2003@gmail.com>
2025-05-20 13:13:32 +0200

    Aggiunto il componente per la prenotazione

--- Codice/Ciscusassi/src/app/pages/prenota/prenota.page.ts
+++ Codice/Ciscusassi/src/app/pages/prenota/prenota.page.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonSpinner} from '@ionic/angular/standalone';
+import { IonSpinner, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader} from '@ionic/angular/standalone';
 import { HeroComponent } from 'src/app/components/hero/hero.component';
 import { LeafletMapComponent } from 'src/app/components/leaflet-map/leaflet-map.component';
 
 import { Filiale } from 'src/app/core/interfaces/Filiale';
 import { PrenotaService } from './prenota.service';
+
 
 @Component({
   selector: 'app-prenota',
