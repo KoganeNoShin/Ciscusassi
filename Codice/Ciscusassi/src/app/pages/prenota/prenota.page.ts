ef4889cd29d4c88cab4e5eddfc07b9bacca128a4
KoganeNoShin <diecoro2003@gmail.com>
2025-05-20 15:58:42 +0200

    Messo Grid in prenotazione

--- Codice/Ciscusassi/src/app/pages/prenota/prenota.page.ts
+++ Codice/Ciscusassi/src/app/pages/prenota/prenota.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonSpinner, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader} from '@ionic/angular/standalone';
+import { IonSpinner, IonContent, IonCardContent, IonInput, IonItem, IonGrid, IonRow, IonCol, IonList, IonAvatar, IonLabel} from '@ionic/angular/standalone';
 import { HeroComponent } from 'src/app/components/hero/hero.component';
 import { LeafletMapComponent } from 'src/app/components/leaflet-map/leaflet-map.component';
 
@@ -9,12 +9,13 @@ import { Filiale } from 'src/app/core/interfaces/Filiale';
 import { PrenotaService } from './prenota.service';
 
 
+
 @Component({
   selector: 'app-prenota',
   templateUrl: './prenota.page.html',
   styleUrls: ['./prenota.page.scss'],
   standalone: true,
-  imports: [IonSpinner, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonContent, CommonModule, FormsModule, HeroComponent, LeafletMapComponent]
+  imports: [IonAvatar, IonList, IonLabel, IonAvatar, IonList, IonCol, IonRow, IonGrid, IonItem, IonInput, IonSpinner, IonCardContent, IonContent, CommonModule, FormsModule, HeroComponent, LeafletMapComponent]
 })
 export class PrenotaPage implements OnInit {
 
