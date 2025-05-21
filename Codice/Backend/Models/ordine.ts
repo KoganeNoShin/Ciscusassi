abc1196ff727a00e3cadc83ef5a3bd323dde59de
KoganeNoShin <diecoro2003@gmail.com>
2025-05-21 15:50:31 +0200

    Aggiornati ord_prod e ordine_ts

---
+++ Codice/Backend/Models/ordine.ts
@@ -0,0 +1,66 @@
+// importo il db
+import db from '../db';
+import { RunResult } from 'sqlite3';
+
+// Definiamo il modello di un Ordine
+export interface OrdineInput {
+    username_ordinante: string;
+    data_ora_ordinazione: string;
+    ref_prenotazione: number;
+    ref_cliente: number;
+    ref_pagamento: number;
+}
+export interface OrdineOutput extends OrdineInput {
+    id_ordine: number; 
+}
+
+// Interagisce direttamente con il database per le operazioni CRUD sugli utenti
+export class Ordine {
+  
+    // definisco il metodo per creare un nuovo utente
+    static async create(data: OrdineInput) : Promise<number>{
+
+        const { username_ordinante, data_ora_ordinazione, ref_prenotazione, ref_cliente, ref_pagamento } = data;
+
+        return new Promise((resolve, reject) => {
+            db.run(
+                'INSERT INTO ordini (id_ordine, username_ordinante, data_ora_ordinazione, ref_prenotazione, ref_cliente, ref_pagamento) VALUES (?, ?, ?, ?, ?, ?)',
+                [username_ordinante, data_ora_ordinazione, ref_prenotazione, ref_cliente, ref_pagamento],
+                function(err) {
+                    if (err) reject(err);
+                    resolve(this.lastID);
+                }
+            );
+        });
+    }
+
+    // definiamo il metodo per ritornare tutti gli ordini
+    static async findAll(): Promise<OrdineOutput[]> {
+        return new Promise((resolve, reject) => {
+            db.all('SELECT * FROM ordini', (err: Error | null, row: OrdineOutput[]) => {
+                if (err) reject(err);
+                resolve(row);
+            });
+        });
+    }
+
+    // ricerca per id
+    static async findById(id: number) : Promise<OrdineOutput> {
+        return new Promise((resolve, reject) => {
+            db.get('SELECT * FROM pagamenti WHERE id_pagamento = ?', [id], (err: Error | null, row: OrdineOutput) => {
+                if (err) reject(err);
+                resolve(row);
+            });
+        });
+    }
+
+    // Aggiorna pagamento
+    /*static async updatePagamento(id: number, idPagamento: number): Promise<RunResult>{
+        return new Promise((resolve, reject) => {
+            db.run('UPDATE pagamenti SET importo = ? WHERE id_pagamento = ?', [id, idPagamento], (err: Error | null, row: RunResult) => {
+                if (err) reject(err);
+                resolve(row);
+            });
+        });
+    }*/
+}
