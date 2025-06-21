20a780fc15397628823defec7b66a9169aca009d
Daniele Susino <susino.daniele@outlook.com>
2025-06-21 13:20:25 +0200

    Palette e browser tab
    Aggiunto colore azzurro in palette.
    Aggiunta icona nella browser tab.
    Aggiunto cambio di titolo nella browser tab.

--- Codice/Ciscusassi/src/app/app.component.ts
+++ Codice/Ciscusassi/src/app/app.component.ts
@@ -7,6 +7,11 @@ import { HeaderComponent } from 'src/app/components/header/header.component';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
 import { AuthenticationService } from './core/services/authentication.service';
 
+import { Title } from '@angular/platform-browser';
+import { filter } from 'rxjs/operators';
+
+import { Router, NavigationEnd } from '@angular/router';
+
 @Component({
 	selector: 'app-root',
 	templateUrl: 'app.component.html',
@@ -15,9 +20,21 @@ import { AuthenticationService } from './core/services/authentication.service';
 export class AppComponent {
 	constructor(
 		private storage: Storage,
-		private authService: AuthenticationService
+		private authService: AuthenticationService,
+		private router: Router,
+		private titleService: Title
 	) {
 		this.initStorage();
+		this.router.events
+			.pipe(filter((event) => event instanceof NavigationEnd))
+			.subscribe(() => {
+				const url = this.router.url;
+				const lastSegment = url.split('/').filter(Boolean).pop();
+				const capitalized = lastSegment
+					? lastSegment[0].toUpperCase() + lastSegment.slice(1)
+					: 'App';
+				this.titleService.setTitle('Ciscusassi - ' + capitalized);
+			});
 	}
 
 	async initStorage() {
