8755583f5d1dbc42e44c8139dd11d7b08632d6cf
Luca Gaetani <luca.gae03@gmail.com>
2025-06-29 22:51:28 +0200

    aggiunta integrazione get ordini chef

--- Codice/Ciscusassi/src/app/pages/cameriere/visualizza-ordini-cameriere/visualizza-ordini-cameriere.page.ts
+++ Codice/Ciscusassi/src/app/pages/cameriere/visualizza-ordini-cameriere/visualizza-ordini-cameriere.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
@@ -6,6 +6,7 @@ import {
 	IonRow,
 	IonCol,
 	IonButton,
+	IonSpinner,
 } from '@ionic/angular/standalone';
 import { OrdProdEstended } from 'src/app/core/interfaces/OrdProd';
 import { ListaOrdiniComponent } from 'src/app/components/lista-ordini/lista-ordini.component';
@@ -23,12 +24,13 @@ import { OrdineService } from 'src/app/core/services/ordine.service';
 		IonCol,
 		IonRow,
 		IonContent,
+		IonSpinner,
 		CommonModule,
 		FormsModule,
 		ListaOrdiniComponent,
 	],
 })
-export class VisualizzaOrdiniCamerierePage implements OnInit {
+export class VisualizzaOrdiniCamerierePage implements OnInit, OnDestroy {
 	private prodottiSubject = new BehaviorSubject<OrdProdEstended[]>([]);
 	public prodotti$: Observable<OrdProdEstended[]> =
 		this.prodottiSubject.asObservable();
@@ -36,14 +38,15 @@ export class VisualizzaOrdiniCamerierePage implements OnInit {
 	tavolo: Tavolo | null = null;
 	private intervalAggiornamento: any;
 
+	// Stato caricamento
+	isLoading = false;
+
 	constructor(
 		private tavoloService: TavoloService,
 		private ordineService: OrdineService
 	) {}
 
-	// Metodo lifecycle Angular chiamato prima che il componente venga distrutto
 	ngOnDestroy() {
-		// Se esiste un intervallo di aggiornamento, lo cancella per evitare memory leak
 		if (this.intervalAggiornamento) {
 			clearInterval(this.intervalAggiornamento);
 		}
@@ -54,16 +57,17 @@ export class VisualizzaOrdiniCamerierePage implements OnInit {
 	}
 
 	ngViewWillEnter() {
-		this.loadOrdini(); // Carica gli ordini inizialmente
+		this.loadOrdini();
 
-		// Imposta un intervallo che ricarica gli ordini ogni 30 secondi
 		this.intervalAggiornamento = setInterval(
 			() => this.loadOrdini(),
 			30000
 		);
 	}
 
-	loadOrdini(){
+	loadOrdini() {
+		this.isLoading = true;
+
 		this.tavolo = this.tavoloService.getTavolo();
 		console.log('Tavolo:', this.tavolo);
 		console.log('Prenotazione:', this.tavolo?.prenotazione);
@@ -71,27 +75,41 @@ export class VisualizzaOrdiniCamerierePage implements OnInit {
 		if (this.tavolo && this.tavolo.prenotazione) {
 			this.ordineService
 				.getProdottiOrdinatiByPrenotazione(this.tavolo.prenotazione)
-				.subscribe((response) => {
-					console.log('Risposta completa dal servizio:', response);
-
-					let prodotti: OrdProdEstended[] = [];
-
-					if (Array.isArray(response)) {
-						prodotti = response;
-					} else if (response && Array.isArray(response.data)) {
-						prodotti = response.data;
-					} else {
-						console.warn(
-							'Struttura della risposta non riconosciuta:',
+				.subscribe({
+					next: (response) => {
+						console.log(
+							'Risposta completa dal servizio:',
 							response
 						);
-					}
 
-					console.log('Prodotti estratti:', prodotti);
-					this.prodottiSubject.next(prodotti);
+						let prodotti: OrdProdEstended[] = [];
+
+						if (Array.isArray(response)) {
+							prodotti = response;
+						} else if (response && Array.isArray(response.data)) {
+							prodotti = response.data;
+						} else {
+							console.warn(
+								'Struttura della risposta non riconosciuta:',
+								response
+							);
+						}
+
+						console.log('Prodotti estratti:', prodotti);
+						this.prodottiSubject.next(prodotti);
+					},
+					error: (err) => {
+						console.error('Errore nel caricamento ordini:', err);
+						this.prodottiSubject.next([]);
+						this.isLoading = false;
+					},
+					complete: () => {
+						this.isLoading = false;
+					},
 				});
 		} else {
 			this.prodottiSubject.next([]);
+			this.isLoading = false;
 		}
 	}
 
